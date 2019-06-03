fd939ff69636ac810f5e5af60691d4008031734f
McM1k <gboudrie@student.42.fr>
2019-06-03 17:06:52 +0200

    reads from a file, then print a vector containing each line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufRead;
+use std::fs::File;
+
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = &args[1];
+
+    let file = File::open(filename)?;
+    let file = std::io::BufReader::new(file);
+
+    let lines: Vec<String> = file.lines().collect::<Result<_, _>>().unwrap();
+    println!("{:?}", lines);
+
+    Ok(())
 }
