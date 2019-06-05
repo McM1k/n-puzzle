bc81093a66d315aa625d8b2c225856b5c6c2a6a0
McM1k <gboudrie@student.42.fr>
2019-06-05 11:55:35 +0200

    split into 3 functions

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
+
+use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 
+fn open_file(filename: String) -> std::io::Result<File> {
+	let file = File::open(filename)?;
 
-fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let filename = &args[1];
+	Ok(file)
+}
 
-    let file = File::open(filename)?;
-    let file = std::io::BufReader::new(file);
+fn file_to_vec(filename: String) -> Vec<String> {
+	let file = open_file(filename).unwrap();
+    let file = BufReader::new(file);
 
     let lines: Vec<String> = file.lines().collect::<Result<_, _>>().unwrap();
-    println!("{:?}", lines);
 
-    Ok(())
+    lines
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = &args[1];
+    let lines = file_to_vec(filename.to_string());
+    println!("{:?}", lines);
 }
