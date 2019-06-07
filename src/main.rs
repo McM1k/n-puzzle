d0ecc31c12599511667aa3fbef29a8bf14daa1f4
McM1k <gboudrie@student.42.fr>
2019-06-07 12:03:11 +0200

    added a few parser functions

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use std::fs::File;
 
 mod parser;
 
-fn open_file(filename: String) -> std::io::Result<File> {
-	let file = File::open(filename)?;
+fn open_file(filename: String) -> File {
+	let file = File::open(filename).expect("Could not open file");
 
-	Ok(file)
+	file
 }
 
 fn file_to_vec(filename: String) -> Vec<String> {
-	let file = open_file(filename).unwrap();
-    let file = BufReader::new(file);
+	let file = open_file(filename);
+    let br_file = BufReader::new(file);
 
-    let lines: Vec<String> = file.lines().collect::<Result<_, _>>().unwrap();
+    let lines: Vec<String> = br_file.lines().collect::<Result<_, _>>().unwrap();
 
     lines
 }
@@ -24,6 +24,10 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let filename = &args[1];
     let mut lines = file_to_vec(filename.to_string());
+    parser::check_empty_lines(&lines);
     lines = parser::remove_comments(lines);
+    parser::check_empty_vec(&lines);
+    parser::check_numbers_or_spaces(&lines);
+    parser::check_size(&lines);
     println!("{:?}", lines);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,57 @@
+pub fn remove_comments(mut lines: Vec<String>) -> Vec<String> { //unsafe if used before check_empty_lines
 
-pub fn remove_comments(mut lines: Vec<String>) -> Vec<String> {
-	
-	lines.retain( |ref l| (
-			l.chars().count() != 0 
-			&& l.chars().nth(0).unwrap() != '#'
-		) 
-		|| l.chars().count() == 0);
+	lines.retain( |ref line| line.chars().nth(0).unwrap() != '#');
+	lines.iter_mut()
+		 .for_each( |ref mut line| {
+		 	let pos = match line.chars().position( |c| c == '#') {
+		 		Some(pos) => pos,
+		 		None	  => line.len(),  
+		 	};
+		 	line.truncate(pos);
+		 });
 
 	lines
 }
+
+pub fn check_empty_lines(lines: & Vec<String>) {
+	lines.iter()
+		 .for_each(|line| if line.is_empty() { panic!("There is an empty line in the file")})
+}
+
+pub fn check_empty_vec(lines: & Vec<String>) {
+	if lines.is_empty() {
+		panic!("No data.")
+	}
+}
+
+pub fn check_numbers_or_spaces(lines: & Vec<String>)  {
+	lines.iter()
+		 .for_each(|line| {
+		 	if !line.chars().all(|c| c.is_digit(10) || c.is_whitespace()) {
+		 		panic!("Unexpected character in file (only spaces and numbers are allowed beside comments)")
+		 	}
+		 })
+}
+
+/*
+fn get_size(lines: & mut Vec<String>) -> u32 {
+	let mut first_digit_line = lines.iter()
+									.nth(0)
+									.unwrap()
+									.split_whitespace();
+	if first_digit_line.clone().count() != 1 {
+		panic!("First line should contain only one number.")
+	}
+	let size = first_digit_line.nth(0)
+							   .unwrap()
+							   .parse::<u32>()
+							   .unwrap();
+	lines.remove(0);
+
+	size
+}
+
+pub fn get_data(lines: Vec<String>, size: u32) -> [[u32; size]; size] { // could fail because fo lifetime
+
+}
+*/
